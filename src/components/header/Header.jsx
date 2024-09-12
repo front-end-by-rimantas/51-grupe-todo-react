f8c57dd57e50b32da403100138d944d9d9cfda28
Rimantas <belauzas@gmail.com>
2024-09-12 09:16:13 +0300

    Header module css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,36 +1,36 @@
 import logo from '../../assets/logo.png';
-import './Header.css';
-import './Button.css';
+import headerStyle from './Header.module.css';
+import buttonStyle from './Button.module.css';
 
 export function Header() {
     return (
-        <header className="main-header">
-            <img className="logo" src={logo} alt="Logo" />
-            <div className="stats">
-                <div className="detail">
-                    <span className="label">Viso:</span>
-                    <span className="value">0</span>
+        <header className={headerStyle.mainHeader}>
+            <img className={headerStyle.logo} src={logo} alt="Logo" />
+            <div className={headerStyle.stats}>
+                <div className={headerStyle.detail}>
+                    <span className={headerStyle.label}>Viso:</span>
+                    <span className={headerStyle.value}>0</span>
                 </div>
-                <div className="detail">
-                    <span className="label">Todo:</span>
-                    <span className="value">0</span>
+                <div className={headerStyle.detail}>
+                    <span className={headerStyle.label}>Todo:</span>
+                    <span className={headerStyle.value}>0</span>
                 </div>
-                <div className="detail">
-                    <span className="label">In-progress:</span>
-                    <span className="value">0</span>
+                <div className={headerStyle.detail}>
+                    <span className={headerStyle.label}>In-progress:</span>
+                    <span className={headerStyle.value}>0</span>
                 </div>
-                <div className="detail">
-                    <span className="label">Done:</span>
-                    <span className="value">0</span>
+                <div className={headerStyle.detail}>
+                    <span className={headerStyle.label}>Done:</span>
+                    <span className={headerStyle.value}>0</span>
                 </div>
-                <div className="detail">
-                    <span className="label">Deleted:</span>
-                    <span className="value">0</span>
+                <div className={headerStyle.detail}>
+                    <span className={headerStyle.label}>Deleted:</span>
+                    <span className={headerStyle.value}>0</span>
                 </div>
             </div>
-            <button className="btn">
-                <span className="icon">+</span>
-                <span className="text">New task</span>
+            <button className={buttonStyle.btn}>
+                <span className={buttonStyle.icon}>+</span>
+                <span className={buttonStyle.text}>New task</span>
             </button>
         </header>
     );
