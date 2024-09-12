3c5780143908976f8851b233eab82785174570ca
Rimantas <belauzas@gmail.com>
2024-09-12 11:00:02 +0300

    Tasks components

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,120 +1,25 @@
+import { TaskCard } from './TaskCard';
 import style from './Tasks.module.css';
 
 export function Tasks() {
     return (
-        <section className="task-content">
-            <div className="column">
-                <h2 className="title">Todo</h2>
-                <ul className="list">
-                    <li className="task">
-                        <div className="header">
-                            <div className="more">...</div>
-                            <div className="more-actions" data-visible="false">
-                                <button type="button">Move up</button>
-                                <button type="button">Move down</button>
-                                <hr />
-                                <button type="button">In progress</button>
-                                <button type="button">Done</button>
-                                <hr />
-                                <button type="button">Delete</button>
-                            </div>
-                        </div>
-                        <div className="content">
-                            <p>Uzduoties tekstas</p>
-                        </div>
-                        <div className="footer">
-                            <p>Deadline: 2024-09-04</p>
-                        </div>
-                    </li>
-                    <li className="task">
-                        <div className="header">
-                            <div className="more">...</div>
-                            <div className="more-actions" data-visible="false">
-                                <button type="button">Move up</button>
-                                <button type="button">Move down</button>
-                                <hr />
-                                <button type="button">In progress</button>
-                                <button type="button">Done</button>
-                                <hr />
-                                <button type="button">Delete</button>
-                            </div>
-                        </div>
-                        <div className="content">
-                            <p>Uzduoties tekstas</p>
-                        </div>
-                        <div className="footer">
-                            <p>Deadline: 2024-09-04</p>
-                        </div>
-                    </li>
-                    <li className="task">
-                        <div className="header">
-                            <div className="more">...</div>
-                            <div className="more-actions" data-visible="false">
-                                <button type="button">Move up</button>
-                                <button type="button">Move down</button>
-                                <hr />
-                                <button type="button">In progress</button>
-                                <button type="button">Done</button>
-                                <hr />
-                                <button type="button">Delete</button>
-                            </div>
-                        </div>
-                        <div className="content">
-                            <p>Uzduoties tekstas: blocked</p>
-                        </div>
-                        <div className="footer">
-                            <p>Deadline: 2024-09-04</p>
-                        </div>
-                    </li>
-                    <li className="task">
-                        <div className="header">
-                            <div className="more">...</div>
-                            <div className="more-actions" data-visible="false">
-                                <button type="button">Move up</button>
-                                <button type="button">Move down</button>
-                                <hr />
-                                <button type="button">In progress</button>
-                                <button type="button">Done</button>
-                                <hr />
-                                <button type="button">Delete</button>
-                            </div>
-                        </div>
-                        <div className="content">
-                            <p>Uzduoties tekstas</p>
-                        </div>
-                        <div className="footer">
-                            <p>Deadline: 2024-09-04</p>
-                        </div>
-                    </li>
-                    <li className="task">
-                        <div className="header">
-                            <div className="more">...</div>
-                            <div className="more-actions" data-visible="false">
-                                <button type="button">Move up</button>
-                                <button type="button">Move down</button>
-                                <hr />
-                                <button type="button">In progress</button>
-                                <button type="button">Done</button>
-                                <hr />
-                                <button type="button">Delete</button>
-                            </div>
-                        </div>
-                        <div className="content">
-                            <p>Uzduoties tekstas</p>
-                        </div>
-                        <div className="footer">
-                            <p>Deadline: 2024-09-04</p>
-                        </div>
-                    </li>
+        <section className={style.taskContent}>
+            <div className={style.column}>
+                <h2 className={style.title}>Todo</h2>
+                <ul className={style.list}>
+                    <TaskCard />
+                    <TaskCard />
+                    <TaskCard />
+                    <TaskCard />
                 </ul>
             </div>
-            <div className="column">
-                <h2 className="title">In progress</h2>
-                <ul className="list">LIST...</ul>
+            <div className={style.column}>
+                <h2 className={style.title}>In progress</h2>
+                <ul className={style.list}>LIST...</ul>
             </div>
-            <div className="column">
-                <h2 className="title">Done</h2>
-                <ul className="list">LIST...</ul>
+            <div className={style.column}>
+                <h2 className={style.title}>Done</h2>
+                <ul className={style.list}>LIST...</ul>
             </div>
         </section>
     );
