04a93dd34d8e8e877db8e7abc95bb29def51a8cf
Rimantas <belauzas@gmail.com>
2024-09-12 10:01:10 +0300

    Stats component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,13 @@
 import logo from '../../assets/logo.png';
 import { Button } from '../button/Button';
-import headerStyle from './Header.module.css';
+import { Stats } from '../stats/Stats';
+import style from './Header.module.css';
 
 export function Header() {
     return (
-        <header className={headerStyle.mainHeader}>
-            <img className={headerStyle.logo} src={logo} alt="Logo" />
-            <div className={headerStyle.stats}>
-                <div className={headerStyle.detail}>
-                    <span className={headerStyle.label}>Viso:</span>
-                    <span className={headerStyle.value}>0</span>
-                </div>
-                <div className={headerStyle.detail}>
-                    <span className={headerStyle.label}>Todo:</span>
-                    <span className={headerStyle.value}>0</span>
-                </div>
-                <div className={headerStyle.detail}>
-                    <span className={headerStyle.label}>In-progress:</span>
-                    <span className={headerStyle.value}>0</span>
-                </div>
-                <div className={headerStyle.detail}>
-                    <span className={headerStyle.label}>Done:</span>
-                    <span className={headerStyle.value}>0</span>
-                </div>
-                <div className={headerStyle.detail}>
-                    <span className={headerStyle.label}>Deleted:</span>
-                    <span className={headerStyle.value}>0</span>
-                </div>
-            </div>
+        <header className={style.mainHeader}>
+            <img className={style.logo} src={logo} alt="Logo" />
+            <Stats />
             <Button text="New task" icon="+" />
         </header>
     );
